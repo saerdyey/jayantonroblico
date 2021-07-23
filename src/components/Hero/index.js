9974caf4708f956465d1baeb6223e29688f70a55
Jay Anton Roblico <jayantonroblico23@gmail.com>
2021-07-23 21:32:23 +0800

    :sparkles: Hero Section

---
+++ src/components/Hero/index.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { Button } from "../ButtonElements";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroH3,
+  HeroP,
+  HeroBtnWrapper,
+  File,
+  Download,
+} from "./HeroElements";
+import Video from "../../videos/video.mp4";
+
+const Hero = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
+  return (
+    <HeroContainer id="home">
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Jay Anton Roblico</HeroH1>
+        {/* <HeroH3>computer engineering student</HeroH3> */}
+        <HeroP>I love software development :)</HeroP>
+        <HeroP>
+          currently seeking for an internship, open for freelance projects, and
+          in training to be a full stack developer.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="download-resume"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+          >
+            Download CV {hover ? <Download /> : <File />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default Hero;
