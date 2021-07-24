ae25f42001ecb898ea88a7599a40064836f8ec8e
Jay Anton Roblico <jayantonroblico23@gmail.com>
2021-07-25 00:04:40 +0800

    :zap: Just a Footer
    meh

---
+++ src/components/Footer/FooterElements.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+  background-color: #010606;
+  color: #fff;
+  margin: 0 auto;
+  padding: 20px 0;
+`;
+export const FooterP = styled.p`
+  text-align: center;
+`;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { FooterContainer, FooterP } from "./FooterElements";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterP>Designed and Built with ♥ by Jay Anton Roblico</FooterP>
+      <FooterP>2021</FooterP>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
