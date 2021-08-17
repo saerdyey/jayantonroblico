4106e059425bc8eb17ad4a584b7b2f8f59039abf
Jay Anton Roblico <jayantonroblico23@gmail.com>
2021-08-18 00:21:05 +0800

    :lipstick: Updated Footer
    footer with links

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -2,10 +2,68 @@ import styled from "styled-components";
 
 export const FooterContainer = styled.div`
   background-color: #010606;
-  color: #fff;
-  margin: 0 auto;
   padding: 20px 0;
 `;
+
+export const FooterWrapper = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media only screen and (max-width: 480px) {
+    flex-direction: column;
+    width: 95%;
+  }
+`;
+
+export const FooterText = styled.div`
+  flex: 1;
+  color: #767676;
+`;
+
+export const FooterTextP = styled.p`
+  text-align: start;
+  @media only screen and (max-width: 480px) {
+    text-align: center;
+  }
+`;
+
+export const FooterCredits = styled.div`
+  flex: 1;
+  text-align: center;
+  color: #fff;
+
+  @media only screen and (max-width: 480px) {
+    padding: 15px 0px;
+  }
+`;
+
+export const FooterLinks = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LinkIcon = styled.img`
+  width: 24px;
+  height: auto;
+  filter: invert(50%) sepia(1%) saturate(0%) hue-rotate(20deg);
+  &:hover {
+    filter: invert(92%) sepia(100%) saturate(36%) hue-rotate(250deg);
+    transform: scale(1.2);
+    cursor: pointer;
+  }
+  transition: all 0.1s;
+  transform-origin: 50% 50%;
+`;
+export const LinkBox = styled.div`
+  padding: 0 10px;
+`;
+
 export const FooterP = styled.p`
   text-align: center;
 `;
+
+export const Anchor = styled.a``;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,60 @@
 import React from "react";
-import { FooterContainer, FooterP } from "./FooterElements";
+import {
+  FooterContainer,
+  FooterWrapper,
+  FooterText,
+  FooterTextP,
+  FooterLinks,
+  LinkBox,
+  LinkIcon,
+  FooterCredits,
+  FooterP,
+  Anchor,
+} from "./FooterElements";
+
+import twitter from "../../images/logos/twitter.svg";
+import facebook from "../../images/logos/facebook.svg";
+import linkedin from "../../images/logos/linkedin.svg";
+import github from "../../images/logos/github.svg";
 
 const Footer = () => {
   return (
     <FooterContainer>
-      <FooterP>Designed and Built with ♥ by Jay Anton Roblico</FooterP>
-      <FooterP>2021</FooterP>
+      <FooterWrapper>
+        <FooterText>
+          <FooterTextP>Wanna work with me?</FooterTextP>
+          <FooterTextP>Ping me up and let's discuss your idea!</FooterTextP>
+        </FooterText>
+        <FooterCredits>
+          <FooterP>Designed and Built with ♥ by Jay Anton Roblico</FooterP>
+          <FooterP>2021</FooterP>
+        </FooterCredits>
+        <FooterLinks>
+          <LinkBox>
+            <Anchor href="https://twitter.com/saerdyey" target="_blank">
+              <LinkIcon src={twitter} />
+            </Anchor>
+          </LinkBox>
+          <LinkBox>
+            <Anchor href="https://facebook.com/saerdyey" target="_blank">
+              <LinkIcon src={facebook} />
+            </Anchor>
+          </LinkBox>
+          <LinkBox>
+            <Anchor
+              href="https://www.linkedin.com/in/jayantonroblico23/"
+              target="_blank"
+            >
+              <LinkIcon src={linkedin} />
+            </Anchor>
+          </LinkBox>
+          <LinkBox>
+            <Anchor href="https://github.com/saerdyey" target="_blank">
+              <LinkIcon src={github} />
+            </Anchor>
+          </LinkBox>
+        </FooterLinks>
+      </FooterWrapper>
     </FooterContainer>
   );
 };
